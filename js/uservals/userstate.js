adf1f96e318bee549c9d7be9e6ba38b03043f5c6
knod <knodding@gmail.com>
2014-07-29 18:50:25 -0400

    Fixes two userStates files

--- js/uservals/userstate.js
+++ js/uservals/userstate.js
@@ -1,43 +1,18 @@
 /*
-* User variables that need to be stored and accessed
+* Variables representing the user, like location and
+* elements displayed.
 */
 
 module.exports = userState = {
 
 	arrival: true,
 
-	// Display
+	// --- HUD --- \\
 	editorShowing: false,
 	inspectorShowing: true,
+	activeHud: null,  // May be sent/retrieve object data
 
-	preferences: {
-
-		hotkeys: {
-
-			// --- FPS Movement --- \\
-			// NOT YET IMPLEMENTED
-			forwards: ["forwards", "w", 87],
-			left: ["left", "a", 65],
-			backwards: ["backwards", "s", 83],
-			right: ["right", "d", 68],
-			jumpup: ["jump/up", "spacebar", 32],  // just up
-			crouchdown: ["crouch/down", "shift", 16],  // just down
-			// TODO: Implement double tapping, holding, and other behavior
-			// TODO: Discuss implications of this behavior for user experience
-			// startFlying: ["start flying", "double tap space bar", 32],
-			toggleFlying: ["toggle flying", "f", 70],
-
-			// --- pointerlock --- \\
-			pointerLock: ["pointer lock", "esc", 27],
-
-			// --- Testing --- \\
-			tests: ["run tests", "t", 84],
-
-		},  // end hotkeys{}
-
-		flying: true,
-
-	},  // end preferences{}
+	// --- Scene --- \\
+	selectedObject: null, // Selected scene object
 
 }
-
