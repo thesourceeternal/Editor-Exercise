202efdc17b1f48a9631745a879b8d2690bcce9a7
knod <knodding@gmail.com>
2014-07-31 19:01:48 -0400

    Attaches and detaches axes correctly when hovering over elements

--- js/controls/select.js
+++ js/controls/select.js
@@ -7,12 +7,20 @@ var transforms = require('./TransformControls.js');
 var userState = require('../uservals/userstate.js');
 
 
+'use strict'
+
 module.exports = select = {
 
 	// Currently selected object is in userstate.js
 	// just so there's one central place to get it from
 
 	axes: null,
+
+	// For getting intersections
+	projector: new THREE.Projector(),
+	raycaster: new THREE.Raycaster(),
+	pointerVector: new THREE.Vector3(),
+	camPosition: new THREE.Vector3(),
 
 	// Add axes to scene, access functions of axes types
 	initSelect: function () {
@@ -67,25 +75,100 @@ module.exports = select = {
 		// If it's a different object then before, select it
 		if ( newObj !== userState.selectedObject ) {
 
+			// console.log( newObj );
+
 			select.selectObject( newObj );
 
 		}
 
 	},  // end selctionHandler()
 
-	// Picks the object closest to the center view of the camera
+	// Returns the object closest to the pointer
 	firstIntersect: function ( event ) {
 
-		console.log("I'm the closest intersected object!");
+		var intersectors = select.pointerIntersectors(event);
+
+		if ( intersectors[0] !== undefined ) {
+
+			return intersectors[0].object;
+
+		} else {
+
+			return undefined;
+
+		}
 
 	},  // end firstIntersect()
 
+	// Returns a list of the objects intersected by the pointer
+	// TODO: Explore only testing visible objects
+	pointerIntersectors: function ( event ) {
+
+		var raycaster = select.raycaster,
+			projector = select.projector,
+			pointerVector = select.pointerVector;
+
+		var camera = cubeWorld.camera;
+
+		var camPosition = select.camPosition;
+		camPosition.setFromMatrixPosition( camera.matrixWorld );
+
+		// Will be pointer coords
+		var x, y;
+
+		// When pointer is free, need to get it's position realtive to canvas
+		if ( userState.editorShowing ) {
+
+			var element = cubeWorld.renderer.domElement;
+			var rect = element.getBoundingClientRect();
+
+			x = (( event.clientX - rect.left ) / rect.width) * 2 - 1;
+			y = -(( event.clientY - rect.top ) / rect.height) * 2 + 1;
+
+		} else {
+			// When pointer is locked, pointer always at center
+			x = 0, y = 0;
+
+		}
+
+		// Right now pointerVector is actually coords in the world
+		pointerVector.set( x, y, 0.5 );
+		// Now pointerVector is the mousen's screen position
+		projector.unprojectVector( pointerVector, camera );
+		// Now it's pointing to the middle of the world again...
+		pointerVector.sub( camPosition ).normalize();
+
+		raycaster.set( camPosition, pointerVector );
+
+		return raycaster.intersectObjects( cubeWorld.scene.children );
+
+	},  // end pointerIntersectors()
+
 	// Sets selected object and shows its axes
 	// axes showing may need to happen in a view related script
-	selectObject: function ( object ) {
+	selectObject: function ( newObject ) {
 
-		console.log("I've selected a new object!");
+		var oldObject = userState.selectedObj;
+		// console.log("oldObject:");
+		// console.log(oldObject);
+		// console.log( "newObject" );
+		// console.log( newObject )
+		// console.log(select.axes)
 
-	}
+		if ( oldObject !== undefined ) {
+
+			select.axes.detach( oldObject );
+
+		}
+
+		userState.selectedObj = newObject;
+
+		if ( newObject !== undefined ) {
+
+			select.axes.attach( newObject );
+
+		}
+
+	},  // end selectObject()
 
 };
