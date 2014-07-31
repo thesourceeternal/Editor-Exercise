42d1a67d6894492b65bb6c3c8b90f79011e55b1c
knod <knodding@gmail.com>
2014-07-31 09:14:31 -0400

    Two next selection functions being called

--- js/controls/select.js
+++ js/controls/select.js
@@ -2,7 +2,7 @@
 * Handles object selection and selectability
 */
 
-var cubeWorld = require('../../server/worlds/cubeworld.js');
+var world = require('../../server/worlds/cubeworld.js');
 var transforms = require('./TransformControls.js');
 var userState = require('../uservals/userstate.js');
 
@@ -10,15 +10,16 @@ var userState = require('../uservals/userstate.js');
 module.exports = select = {
 
 	// Currently selected object is in userstate.js
+	// just so there's one central place to get it from
 
 	axes: null,
 
 	// Add axes to scene, access functions of axes types
 	initSelect: function () {
 
-		var renderer = cubeWorld.renderer,
-			camera = cubeWorld.camera,
-			scene = cubeWorld.scene;
+		var renderer = world.renderer,
+			camera = world.camera,
+			scene = world.scene;
 
 		// Set up all the prototypes and transform objects
 		transforms();
@@ -59,10 +60,32 @@ module.exports = select = {
 	// --- Selection --- \\
 
 	// Determines and, if needed sets, the object currently being selected
-	selctionHandler: function (event) {
+	selctionHandler: function ( event ) {
 
-		console.log("In selectionHandler()");
+		// Potential new selected object
+		var newObj = select.firstIntersect(event);
+		// If it's a different object then before, select it
+		if ( newObj !== userState.selectedObject ) {
+
+			select.selectObject( newObj );
+
+		}
 
 	},  // end selctionHandler()
+
+	// Picks the object closest to the center view of the camera
+	firstIntersect: function ( event ) {
+
+		console.log("I'm the closest intersected object!");
+
+	},  // end firstIntersect()
+
+	// Sets selected object and shows its axes
+	// axes showing may need to happen in a view related script
+	selectObject: function ( object ) {
+
+		console.log("I've selected a new object!");
+
+	}
 
 };
